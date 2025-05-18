69f2546fdb32a1ead689520daed326419033ff86
obstacle-Sweden-Avicii <3485793523@qq.com>
2025-05-19 00:49:43 +0800

    obstacle(厨师长

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -1,5 +1,11 @@
 export default [
   {
+    id: "obstacle",
+    intro: "24级crypto / 厨师长，负责端茶倒水",
+    avatar: require("../../images/avatar/obstacle.jpg"),
+    url: "https://obstacle.top/"
+  },
+  {
     id: "Godz1lla",
     intro: "23级 / 最抽象的会长生物 / 致敬传奇摆烂王",
     avatar: require("../../images/avatar/Godz1lla.png"),
