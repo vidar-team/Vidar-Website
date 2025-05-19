51038f96117147f5f42cb9e4d8cb8e76de433845
DESKTOP-PP7V2VN\Wsdx233 <18082792459@163.com>
2025-05-19 18:41:21 +0800

    add wsdx233

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -1,5 +1,11 @@
 export default [
   {
+    id: "Wsdx233",
+    intro: "24级 / 游戏开发 / 学习逆向",
+    avatar: require("../../images/avatar/wsdx233.jpg"),
+    url: "#"
+  },
+  {
     id: "Godz1lla",
     intro: "23级 / 最抽象的会长生物 / 致敬传奇摆烂王",
     avatar: require("../../images/avatar/Godz1lla.png"),
