34fcb8ee9175adb0d593cf130f2fa7a113ce62b7
whi4ed0g <2253577386@QQ.com>
2025-05-19 19:14:09 +0800

    Add whi4ed0g

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -1,5 +1,11 @@
 export default [
   {
+    id: "whi4ed0g",
+    intro: "24级 / 菜鸡pwn手 ",
+    avatar: require("../../images/avatar/whi4ed0g.jpg"),
+    url: "https://whi4ed0g.xyz/"
+  },
+  {
     id: "Godz1lla",
     intro: "23级 / 最抽象的会长生物 / 致敬传奇摆烂王",
     avatar: require("../../images/avatar/Godz1lla.png"),
