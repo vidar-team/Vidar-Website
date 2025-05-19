3e38a47bbe036833eab247e31cd1dc6fa079d8c3
re35t <2467065660@qq.com>
2025-05-19 21:05:02 +0800

    Add re35T

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -1,5 +1,11 @@
 export default [
   {
+    id: "re35T",
+    intro: "24级 / 菜菜Web / 喜欢学看不懂的 / 佛系猫奴",
+    avatar: require("../../images/avatar/re35T.png"),
+    url: "http://re35t.top"
+  },
+  {
     id: "Godz1lla",
     intro: "23级 / 最抽象的会长生物 / 致敬传奇摆烂王",
     avatar: require("../../images/avatar/Godz1lla.png"),
