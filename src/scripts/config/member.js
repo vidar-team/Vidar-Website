b26ba6f1fbd7d7e1ceffdd4b818729d9d72ac835
Shiori <mojin5347@gmail.com>
2025-05-21 11:31:37 +0800

    Add Shiori (#21)
    Co-authored-by: Sh10rl <mojin5347@163.com>
    Co-authored-by: Woshiluo Luo <woshiluo.luo@outlook.com>

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -1,10 +1,17 @@
 export default [
   {
+    id: "Shiori",
+    intro: "24级 / re / 菜 / 还要进步",
+    avatar: require("../../images/avatar/Shiori.png"),
+    url: "sh10rl.top"
+  },
+  {
     id: "Woore",
     intro: "24级 / 努力驯化agent的歪脖手",
     avatar: require("../../images/avatar/Woore.jpg"),
     url: "http://www.woore.website/wordpress/"
   },
+  {
     id: "Wsdx233",
     intro: "24级 / 游戏开发 / 学习逆向",
     avatar: require("../../images/avatar/wsdx233.jpg"),
